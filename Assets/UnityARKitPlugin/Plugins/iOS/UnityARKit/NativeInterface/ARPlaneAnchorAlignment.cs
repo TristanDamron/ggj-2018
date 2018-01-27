33bb8b6d5bf92b9c00e7621f22e5325f7d135d7a
lauri-rustanius <lauri.rustanius@mapbox.com>
2018-01-26 23:17:43 -0800

    updated unityARinterface

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/NativeInterface/ARPlaneAnchorAlignment.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/NativeInterface/ARPlaneAnchorAlignment.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 
 namespace UnityEngine.XR.iOS
 {
-	public enum ARPlaneAnchorAlignment : long
-	{
-		/** A plane that is horizontal with respect to gravity. */
-		ARPlaneAnchorAlignmentHorizontal
-	}
+    public enum ARPlaneAnchorAlignment : long
+    {
+        /** A plane that is horizontal with respect to gravity. */
+        ARPlaneAnchorAlignmentHorizontal,
+        ARPlaneAnchorAlignmentVertical
+    }
 }
 
