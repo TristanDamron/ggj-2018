8fa509887e0f57a87ebc26582c2d500f1b55cb4b
lauri-rustanius <lauri.rustanius@mapbox.com>
2018-01-27 04:02:57 -0800

    setup scene

---
+++ Assets/Creaturez/Scripts/JointScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointScript : MonoBehaviour {
+    
+    [SerializeField]
+    private Rigidbody _anchor;
+    private SpringJoint _joint;
+
+
+    public void Grabbed()
+    {
+        CreateAJoint();
+    }
+
+    void DestroyJoint()
+    {
+        Destroy(GetComponent<SpringJoint>());
+    }
+
+    void CreateAJoint()
+    {
+        
+        gameObject.AddComponent<SpringJoint>();
+        _joint = GetComponent<SpringJoint>();
+        _joint.connectedBody = _anchor;
+        _joint.spring = 75f;
+        _joint.damper = 0.3f;
+        _joint.tolerance = 0f;
+        _joint.enablePreprocessing = true;
+    }
+
+    public Transform ReturnAnchorTransform()
+    {
+        return _anchor.GetComponent<Transform>();
+    }
+}
