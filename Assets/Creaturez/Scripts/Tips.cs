17d9d808c6a16f668ae489159b1db222857a2171
Tristan Damron <TristanDamron31@gmail.com>
2018-02-14 13:07:31 -0800

    Added game tips.

---
+++ Assets/Creaturez/Scripts/Tips.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tips : MonoBehaviour {
+	public enum  Events
+	{
+		Patrolling,
+		PickedUpBird,
+		FightingMonster
+	}
+
+	public Events current;
+	[SerializeField]
+	private Image _img;
+
+	[SerializeField]
+	private Sprite[] _images;
+ 
+	void Start() {
+		_img = GetComponent<Image>();
+	}
+
+	private void SetImage() {
+		if (current == Events.Patrolling) {
+			_img.sprite = _images[0]; 
+		} else if (current == Events.PickedUpBird) {
+			_img.sprite = _images[1]; 
+		} else if (current == Events.FightingMonster) {
+			_img.sprite = _images[2];  
+		}
+	}
+
+	void Update () {
+		Invoke("SetImage", 1f);
+	}
+}
