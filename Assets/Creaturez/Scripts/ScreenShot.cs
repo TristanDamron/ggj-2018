ec8fde6a441817c62636a57de594155c8490a9a2
Tristan Damron <TristanDamron31@gmail.com>
2018-02-22 20:45:30 -0800

    Added a screenshot button.

---
+++ Assets/Creaturez/Scripts/ScreenShot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScreenShot : MonoBehaviour {
+	public void TakeScreenShot() {
+		ScreenCapture.CaptureScreenshot(Application.persistentDataPath + "/image" + Random.Range(100000f, 1000000f) + ".png");
+	}
+}
